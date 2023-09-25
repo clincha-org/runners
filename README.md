dfe4d1f8856eb37b5c12367142d2c5da3259a339
clincha <angus.clinch@gmail.com>
2023-09-25 22:21:53 +0100

    documentation outline

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # runners
+
 Runners for the organisation
+
+Following along
+with [the documentation](https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners-with-actions-runner-controller/quickstart-for-actions-runner-controller)
+
+There are two components
+
+- Actions runner controller
+- Runner scale sets
+
+I need to set up an organisation runner scale
+set. [Documentation](https://github.com/actions/actions-runner-controller/blob/master/docs/choosing-runner-destination.md)
+for that is kept separate.
---
+++ manual-install.sh
@@ -0,0 +1,16 @@
+NAMESPACE="arc-systems"
+helm install arc \
+    --namespace "${NAMESPACE}" \
+    --create-namespace \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+INSTALLATION_NAME="arc-runner-set"
+NAMESPACE="arc-runners"
+GITHUB_CONFIG_URL="https://github.com/<your_enterprise/org/repo>"
+GITHUB_PAT="<PAT>"
+helm install "${INSTALLATION_NAME}" \
+    --namespace "${NAMESPACE}" \
+    --create-namespace \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
